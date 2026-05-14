3459ffce8fd8ed3603276f36e41155606b84baab
shambhaviholay1983 <shambhavi.holay1983@gmail.com>
2026-05-14 16:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import os
+from flask import Flask
 
-if __name__ == "__main__":
-    # Render कडून मिळणारा पोर्ट वापरा किंवा डिफॉल्ट 5000 वापरा
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+# ही ओळ तपासा. 'app' हे नाव लहान अक्षरात (lowercase) असणे आवश्यक आहे.
+app = Flask(__name__) 
+
+@app.route('/')
+def index():
+    return "Hello World"
