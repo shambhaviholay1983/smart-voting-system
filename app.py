44bb803bdc04d270bf8b56939747fe956f7e647a
shambhaviholay1983 <shambhavi.holay1983@gmail.com>
2026-05-16 20:58:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
 # ही ओळ तपासा. 'app' हे नाव लहान अक्षरात (lowercase) असणे आवश्यक आहे.
 #app = Flask(__name__) 
@@ -7,6 +7,8 @@ from flask import Flask, render_template
 #def index():
 #    return "Hello World phd"
 
+# This imports everything or specific items from your voting_manager.py file
+from flask import Flask, render_template
 # This imports everything or specific items from your voting_manager.py file
 import voting_manager 
 # OR: from voting_manager import verify_voter_biometrics, initialize_voting_db
